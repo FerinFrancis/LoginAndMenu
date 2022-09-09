e088394e474d66491d664649087d8eb3795f1068
ferin <ferinfrancis1997@gmail.com>
2022-09-09 15:32:19 +0530

    notification update

--- app/src/main/java/com/ferin/loginandmenu/MainActivity.java
+++ app/src/main/java/com/ferin/loginandmenu/MainActivity.java
@@ -2,50 +2,77 @@ package com.ferin.loginandmenu;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
+
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+import android.widget.VideoView;
 
-import java.lang.reflect.Array;
+import com.ferin.loginandmenu.databinding.ActivityMainBinding;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText nameEdit, mobileEdit,locationEdit,emailEdit;
+    ActivityMainBinding actBind;
+
+    EditText nameEdit, mobileEdit,emailEdit,locationEdit;
     CheckBox agreeCheck;
-    Button toastButtonVar,textViewButton, goNextButton;
     String name, mobile,location,email,data;
+    DatePickerDialog dobPicker;
+    TimePickerDialog tobPicker;
+    TextView dobTextView, tobTextView;
+    String dob,tob;
+    MediaPlayer mediaPlayer;
+    String spinnerData;
+
+    ArrayList spinnerArrayList;
+    ArrayAdapter spinnerArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        actBind = DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
 
         nameEdit = findViewById(R.id.nameEnterEdit);
         mobileEdit = findViewById(R.id.mobileEnterEdit);
         locationEdit = findViewById(R.id.locationEnterEdit);
         emailEdit = findViewById(R.id.emailEnterEdit);
+        dobTextView = findViewById(R.id.dobSelectTextView);
+        tobTextView = findViewById(R.id.tobSelectTextView);
+
 
         agreeCheck = findViewById(R.id.agreeCheckBox);
-        toastButtonVar = findViewById(R.id.toastButton);
-        textViewButton = findViewById(R.id.textViewButton);
-        goNextButton = findViewById(R.id.goToNext);
+
+        Spinner();
 
 
         // When this button is clicked, the data from editText is shown as a toast
-        toastButtonVar.setOnClickListener(new View.OnClickListener() {
+        actBind.toastButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // When button is clicked, it stores the data from all editTexts to respective variable
@@ -53,12 +80,15 @@ public class MainActivity extends AppCompatActivity {
                 mobile = mobileEdit.getText().toString();
                 location = locationEdit.getText().toString();
                 email = emailEdit.getText().toString();
+                dob = dobTextView.getText().toString();
+                tob = tobTextView.getText().toString();
+                spinnerData = actBind.spinner.getSelectedItem().toString();
 
                 // And then it verifies if checkbox is in checked state
                 if(agreeCheck.isChecked()){
 
                     // If it is in checked state, proceeds to checks if any field is empty or not
-                    if (TextUtils.isEmpty(name)||TextUtils.isEmpty(mobile)||TextUtils.isEmpty(location)||TextUtils.isEmpty(email))
+                    if (TextUtils.isEmpty(name)||TextUtils.isEmpty(mobile)||TextUtils.isEmpty(location)||TextUtils.isEmpty(email)||TextUtils.isEmpty(dob)||TextUtils.isEmpty(tob))
                     {
                         // If any one field is empty, this toast message is shown
                         Toast.makeText(MainActivity.this, "Empty field not allowed!", Toast.LENGTH_SHORT).show();
@@ -66,7 +96,8 @@ public class MainActivity extends AppCompatActivity {
                     else
                     {
                         // If all field has data, these steps gets executed and toast is shown
-                    data = "Name: "+name+" \nMobile: "+mobile+" \nLocation: "+location+" \nEmail: "+email;
+//                    data = "Name: "+name+" \nMobile: "+mobile+" \nLocation: "+location+" \nEmail: "+email;
+                        data = "Name: "+name+"\nMobile: "+mobile+"\nLocation: "+location+"\nEmail: "+email+"\nQualification: "+spinnerData+"\nD.O.B: "+dob+"\nT.O.B: "+tob;
                     Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
                     }
                 }
@@ -74,20 +105,24 @@ public class MainActivity extends AppCompatActivity {
                 {
                     // If checkbox is not checked, this toast is shown
                     Toast.makeText(MainActivity.this, "Please Agree and Retry", Toast.LENGTH_SHORT).show();
+                    mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.sorryaudiofile);
+                    mediaPlayer.start();
                 }
-
             }
         });
 
 
         // When this button is clicked, the data from editText is shown in a AlertDialog Box
-        textViewButton.setOnClickListener(new View.OnClickListener() {
+        actBind.textViewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 name = nameEdit.getText().toString();
                 mobile = mobileEdit.getText().toString();
                 location = locationEdit.getText().toString();
                 email = emailEdit.getText().toString();
+                spinnerData = actBind.spinner.getSelectedItem().toString();
+
+
                 if(agreeCheck.isChecked()) {
 
                     // If it is in checked state, proceeds to checks if any field is empty or not
@@ -104,7 +139,8 @@ public class MainActivity extends AppCompatActivity {
                         // set the title which has to be displayed in alert dialog box
                         data.setTitle("Title Here");  // Not mandatory
                         // set the message which has to be displayed
-                        data.setMessage("Message is shown here:\nName: "+name+" \nMobile: "+mobile+" \nLocation: "+location+" \nEmail: "+email);
+//                        data.setMessage("Message is shown here:\nName: "+name+" \nMobile: "+mobile+" \nLocation: "+location+" \nEmail: "+email);
+                        data.setMessage("Message is shown here:\nName: "+name+"\nMobile: "+mobile+"\nLocation: "+location+"\nEmail: "+email+"\nQualification: "+spinnerData+"\nD.O.B: "+dob+"\nT.O.B: "+tob);
                         // Set the icon which has to be shown next to Title
                         data.setIcon(android.R.drawable.star_big_on);  // By default icon is set next to Title
                         data.setPositiveButton("Positive",null); // set text to right side of dialog box
@@ -117,12 +153,15 @@ public class MainActivity extends AppCompatActivity {
                 else
                 {
                     Toast.makeText(MainActivity.this, "Please Agree and Retry", Toast.LENGTH_SHORT).show();
+                    mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.sorryaudiofile);
+                    mediaPlayer.start();
                 }
             }
         });
 
+
         // When this button is clicked, it shows a AlertDialog and upon clicking "Yes" it proceeds to next activity
-        goNextButton.setOnClickListener(new View.OnClickListener() {
+        actBind.goToNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -131,6 +170,9 @@ public class MainActivity extends AppCompatActivity {
                 mobile = mobileEdit.getText().toString();
                 location = locationEdit.getText().toString();
                 email = emailEdit.getText().toString();
+                dob = dobTextView.getText().toString();
+                tob = tobTextView.getText().toString();
+                spinnerData = actBind.spinner.getSelectedItem().toString();
 
                 // And then it verifies if checkbox is in checked state
                 if(agreeCheck.isChecked()){
@@ -157,6 +199,9 @@ public class MainActivity extends AppCompatActivity {
                                 intent.putExtra("Mobile",mobile);
                                 intent.putExtra("Location",location);
                                 intent.putExtra("Email",email);
+                                intent.putExtra("DOB",dob);
+                                intent.putExtra("TOB",tob);
+                                intent.putExtra("Qualif",spinnerData);
                                 startActivity(intent);
                             }
                         }).show();
@@ -165,9 +210,34 @@ public class MainActivity extends AppCompatActivity {
                 else
                 {
                     Toast.makeText(MainActivity.this, "Please Agree and Retry", Toast.LENGTH_SHORT).show();
+                    mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.sorryaudiofile);
+                    mediaPlayer.start();
                 }
             }
         });
+
+        actBind.wifiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.Q){
+                    Intent intent = new Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Wifi enabled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        actBind.bluetoothButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
     }
 
     // This method is used to create Menu
@@ -186,10 +256,97 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Settings will be opened later!", Toast.LENGTH_SHORT).show();
     }
 
+
+
     // To create a customDialog
     public void customDialog(View view) {
         Dialog dia = new Dialog(MainActivity.this); // Dialog is the predefined classname for customDialog
-        dia.setContentView(R.layout.activity_thank_you); // This displays activity_thank_you.xml in the customDialogBox
+
+
+        dia.setContentView(R.layout.video_layout); // This displays xml layout in the customDialogBox
+        VideoView video = dia.findViewById(R.id.videoView);
+        Uri uri = Uri.parse("https://www.ebookfrenzy.com/android_book/movie.mp4");
+  //      Uri uri = Uri.parse("https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"); // Not Working
+        video.setVideoURI(uri);
+        video.start();
+
+//        USE THIS IF dia.setContentView(R.layout.activity_thank_you) is used
+//        dia.findViewById(R.id.thanksActExitButton).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Working", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+                dia.show();
+    }
+
+    public void customDialogTwo(View view) {
+        Dialog dia2 = new Dialog(MainActivity.this);
+        dia2.setContentView(R.layout.video_button_layout);
+        VideoView video2 = dia2.findViewById(R.id.videoView2);
+        dia2.findViewById(R.id.videoView2Button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.ebookfrenzy.com/android_book/movie.mp4");
+                video2.setVideoURI(uri);
+                video2.start();
+            }
+        });
+        dia2.show();
+    }
+
+
+    public void SelectDOB(View view) {
+
+        // To set the current date to be set in the datePickerDialogBox
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // This creates a datePickerDialogBox
+        dobPicker = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                dobTextView.setText(i2+"/"+(i1+1)+"/"+i);
+            }
+        }, mYear, mMonth, mDay);
+        dobPicker.show();
+    }
+
+    public void SelectTOB(View view) {
+
+        // To set the current time to be set in the timePickerDialogBox
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR);
+        int minute = c.get(Calendar.MINUTE);
+
+        // This creates a timePickerDialogBox
+        tobPicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                tobTextView.setText(i+":"+i1);
+            }
+        }, hour, minute, true);
+        tobPicker.show();
+    }
+
+    public void CustomGoogleDialog(MenuItem item) {
+        Dialog dia = new Dialog(MainActivity.this);
+        dia.setContentView(R.layout.custom_dialogbox);
         dia.show();
     }
+
+     void Spinner(){
+        spinnerArrayList = new ArrayList();
+        spinnerArrayList.add("Select an Option");
+        spinnerArrayList.add("B.E/B.Tech");
+        spinnerArrayList.add("B.Sc");
+        spinnerArrayList.add("B.Com");
+        spinnerArrayList.add("Diploma");
+
+        spinnerArrayAdapter = new ArrayAdapter(this,R.layout.item_list,spinnerArrayList);
+        actBind.spinner.setAdapter(spinnerArrayAdapter);
+    }
+
 }
--- app/src/main/java/com/ferin/loginandmenu/MainActivity2.java
+++ app/src/main/java/com/ferin/loginandmenu/MainActivity2.java
@@ -1,46 +1,156 @@
 package com.ferin.loginandmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ferin.loginandmenu.databinding.ActivityMain2Binding;
+
+import java.util.ArrayList;
+
+// Doubt: what is request code in SMS
+// SMS permission dialog box displays in MainActivity1 and not in MainActivity2
+// Use of message/rfc822
+
 public class MainActivity2 extends AppCompatActivity {
 
-    TextView nameText,mobileText,locationText,emailText;
-    String name, mobile, location, email;
+    ActivityMain2Binding actbind2;
+    String name, mobile, location, email, dob, tob, qualif;
+    ArrayList arrayList;
+    ArrayAdapter arrayAdapter;
+    AlertDialog.Builder alertDialog;
+    SmsManager smsManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        actbind2 = DataBindingUtil.setContentView(this, R.layout.activity_main2);
 
-        nameText = findViewById(R.id.nameTextView);
-        mobileText = findViewById(R.id.mobileTextView);
-        locationText = findViewById(R.id.locationTextView);
-        emailText = findViewById(R.id.emailTextView);
+
+
+        arrayList = new ArrayList();
 
         name = getIntent().getStringExtra("Name");
-        nameText.setText(name);
+        arrayList.add("Name: "+name);
 
         mobile = getIntent().getStringExtra("Mobile");
-        mobileText.setText(mobile);
+        arrayList.add("Mobile: "+mobile);
+
+        dob = getIntent().getStringExtra("DOB");
+        arrayList.add("D.O.B: "+dob);
+
+        tob = getIntent().getStringExtra("TOB");
+        arrayList.add("T.O.B: "+tob);
+
+        qualif = getIntent().getStringExtra("Qualif");
+        arrayList.add("Occupation: "+qualif);
 
         location = getIntent().getStringExtra("Location");
-        locationText.setText(location);
+        arrayList.add("Location: "+location);
 
         email = getIntent().getStringExtra("Email");
-        emailText.setText(email);
+        arrayList.add("Email: "+email);
 
+
+
+        arrayAdapter = new ArrayAdapter(this, R.layout.item_list,arrayList);
+        actbind2.listView.setAdapter(arrayAdapter);
+
+        actbind2.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==1){
+                    alertDialog = new AlertDialog.Builder(MainActivity2.this);
+                    alertDialog.setMessage("Select an Action");
+                    alertDialog.setPositiveButton("Exit App", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finishAffinity();
+                        }
+                    });
+                    alertDialog.setNegativeButton("Send SMS", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            // Code to check if permission is granted already
+
+                                ActivityCompat.requestPermissions(MainActivity2.this, new String[]{Manifest.permission.SEND_SMS}, 123); // This is the only mandatory line
+
+                        }
+                    });
+                    alertDialog.setNeutralButton("Email", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+                            emailIntent.putExtra(Intent.EXTRA_EMAIL,"ferinfrancis1997@gmail.com"); // If single To-Address is there
+//                            emailIntent.putExtra(Intent.EXTRA_EMAIL,new String[]{"ferinfrancis1997@gmail.com","testmail@gmail.com"}); // If multiple To-Address is there, storing it in String array
+                            emailIntent.putExtra(Intent.EXTRA_SUBJECT,"Test Mail");
+                            emailIntent.putExtra(Intent.EXTRA_TEXT,"This is a test mail");
+
+                            // Need this line to display all the apps that helps to send mails
+                            // If message/rfc822 is not added, no apps will be shown
+                            // If only message/rfc is added, still it works but gmail isn't shown in first in suggestions.
+                            // If you want gmail to be shown in front, message/rfc822 has to be entered completely
+                            emailIntent.setType("message/rfc822");
+
+                            startActivity(Intent.createChooser(emailIntent,"Choose an email client: "));
+                        }
+                    });
+                    alertDialog.show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 123){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(mobile,null,"Saved",null,null);
+
+                NotificationManager notificationManager = (NotificationManager) MainActivity2.this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+                String channelID = "Channel01";
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel(channelID,"SmsConfirmaiton",NotificationManager.IMPORTANCE_DEFAULT);
+                    notificationManager.createNotificationChannel(channel);
+                }
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity2.this,channelID)
+                        .setSmallIcon(android.R.drawable.ic_media_play).setContentTitle("Sms Confirmation").setContentText("Send has been send from this device");
+
+                notificationManager.notify(01,builder.build());
+
+            }
+        }
     }
 
     @Override
